beae3f6a43b8aebcbb0cd8461929540d3103f07a
Claudiney Nascimento <contato@claudiney.info>
2022-12-10 17:03:48 -0500

    Change quip language according to param "lang"

--- quip/src/main/java/com/redhat/training/example/Quip.java
+++ quip/src/main/java/com/redhat/training/example/Quip.java
@@ -4,6 +4,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.DefaultValue;
+
 import java.net.InetAddress;
 
 
@@ -12,10 +15,17 @@ public class Quip {
 
   @GET
   @Produces("text/plain")
-  public Response index() throws Exception {
+  public Response index(
+		    @QueryParam("lang") 
+		    @DefaultValue("en") 
+		    final String lang
+	) throws Exception {
     String host = InetAddress.getLocalHost().getHostName();
-    // return Response.ok("Veni, vidi, vici...\n").build();
-    return Response.ok("I came, I saw, I conquered...\n").build();
+    switch(lang) {
+      case "lat": return Response.ok("Veni, vidi, vici...\n").build();
+      case "pt":  return Response.ok("Vim, vi, venci...\n").build();
+      default:    return Response.ok("I came, I saw, I conquered...\n").build();
+    }
   }
 
   @GET
