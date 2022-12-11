09be3f52abe6deac7095a9296ad4c44fc98542c1
Claudiney Nascimento <contato@claudiney.info>
2022-12-11 20:51:34 -0300

    Update

--- java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
+++ java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
@@ -15,12 +15,10 @@ public class ServerHostEndPoint {
     String host = "";
     try {
       host = InetAddress.getLocalHost().getHostName();
-    }
-    catch (Exception e) {
-       e.printStackTrace();
+    } catch (Exception e) {
        host = Thread.currentThread().getName();
     }
-    String msg = "I am running on server "+host+" Version 1.0 \n";
+    String msg = "I am running on server "+host+" Version 2.0 \n";
     return Response.ok(msg).build();
   }
 }
